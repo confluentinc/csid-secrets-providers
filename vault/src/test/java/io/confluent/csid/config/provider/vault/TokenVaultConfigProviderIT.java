de6d13824c75be675ac0ab031503e0af9015dfb6
stephenheg <shegarty@confluent.io>
2023-08-15 12:36:40 +0100

    CCET-338 fix vault integration tests with new docker image

--- vault/src/test/java/io/confluent/csid/config/provider/vault/TokenVaultConfigProviderIT.java
+++ vault/src/test/java/io/confluent/csid/config/provider/vault/TokenVaultConfigProviderIT.java
@@ -132,7 +132,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TokenVaultConfigProviderIT extends VaultConfigProviderIT {
 
-  final VaultContainer<?> vaultContainer = new VaultContainer<>("vault:latest")
+  final VaultContainer<?> vaultContainer = new VaultContainer<>(HASHICORP_VAULT_DOCKER_IMAGE)
       .withVaultToken("kxbfgiertgibadsf")
       .withNetworkAliases("vault")
       .withExposedPorts(8200);
