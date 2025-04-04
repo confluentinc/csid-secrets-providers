b97b425c629df526f3f4086388af9711c9478fd8
zamlythaha <62118332+zamlythaha@users.noreply.github.com>
2025-04-04 05:57:15 -0700

    Schedule token renewal based on ttl (#344)
    * Schedule token renewal based on ttl
    
    * Set the schedule period to less than the ttl to avoid race conditions
    
    * Fix formatting

--- vault/src/main/java/io/confluent/csid/config/provider/vault/VaultClientImpl.java
+++ vault/src/main/java/io/confluent/csid/config/provider/vault/VaultClientImpl.java
@@ -127,28 +127,39 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 class VaultClientImpl implements VaultClient {
   private static final Logger log = LoggerFactory.getLogger(VaultClientImpl.class);
   final AtomicReference<Vault> vaultStore = new AtomicReference<>();
   final AtomicReference<AuthHandler.AuthResult> authResultStore = new AtomicReference<>();
-  final VaultConfigProviderConfig config;
-  final ScheduledExecutorService executorService;
 
 
   public VaultClientImpl(VaultConfigProviderConfig config, ScheduledExecutorService executorService) {
-    this.config = config;
-    this.executorService = executorService;
 
     VaultConfig vaultConfig = config.createConfig();
     log.info("ctor() - creating initial vault client");
 
-    AuthHandler authHandler = AuthHandlers.get(config.authMethod);
     Vault initialVault = Vault.create(vaultConfig);
+    this.vaultStore.set(initialVault);
+    AuthHandler.AuthResult result = authenticateVault(config, vaultConfig);
+
+    if (result.ttl().isPresent() && result.ttl().get() > 0) {
+      log.debug("ctor() - AuthResult does have a ttl - scheduling token renewal.");
+      executorService.scheduleAtFixedRate(() -> authenticateVault(config, vaultConfig),
+              0, result.ttl().get() - (result.ttl().get() / 10), TimeUnit.SECONDS);
+    } else {
+      log.debug("ctor() - AuthResult does not have a ttl so not scheduling token refresh.");
+    }
+  }
+
+  private AuthHandler.AuthResult authenticateVault(VaultConfigProviderConfig config, VaultConfig vaultConfig) {
+    log.debug("ctor() - authenticating vault");
+    AuthHandler authHandler = AuthHandlers.get(config.authMethod);
     AuthHandler.AuthResult result;
     try {
-      result = authHandler.execute(config, initialVault);
+      result = authHandler.execute(config, this.vaultStore.get());
     } catch (VaultException exception) {
       log.error("ctor() - exception thrown during initial authentication", exception);
       ConfigException configException = new ConfigException("Exception during initial authentication");
@@ -157,28 +168,7 @@ class VaultClientImpl implements VaultClient {
     }
     log.debug("ctor() - authResult = {}", result);
     result.token().ifPresent(vaultConfig::token);
-    this.vaultStore.set(initialVault);
-
-    if (result.ttl().isPresent() && result.ttl().get() > 0) {
-      log.debug("ctor() - AuthResult does have a ttl - ignoring.");
-    } else {
-      log.debug("ctor() - AuthResult does not have a ttl so not scheduling token refresh.");
-    }
-  }
-
-  class TokenRenewer implements Runnable {
-    @Override
-    public void run() {
-
-    }
-  }
-
-  static class AuthRokenRenewer implements Runnable {
-
-    @Override
-    public void run() {
-
-    }
+    return result;
   }
 
 
